a3b933f582e1c1e771f8dd3ace0ac9bb77456a94
starvis <starv0411@gmail.com>
2021-05-12 22:07:03 +0200

    added Test Coverage for line 18 false branch of BookmarkTool

--- src/test/java/at/ac/fhcampuswien/csdc21bb/group3/domain/BookmarkToolTest.java
+++ src/test/java/at/ac/fhcampuswien/csdc21bb/group3/domain/BookmarkToolTest.java
@@ -28,6 +28,19 @@ class BookmarkToolTest {
     }
 
     @Test
+    public void ensureAddingBookmarkWhichIsNotInTheBookmarksList() throws MalformedURLException {
+        Bookmark b1 = new Bookmark("https://www.orf.at");
+        Bookmark b2 = new Bookmark("https://www.fh-campuswien.ac.at");
+        BookmarkTool bookmarkTool = new BookmarkTool();
+
+        bookmarkTool.addBookmark(b1);
+        bookmarkTool.addBookmark(b1);
+        int listSize = bookmarkTool.addBookmark(b2);
+
+        assertEquals(2, listSize);
+    }
+
+    @Test
     public void increaseRatingOfBookmarkWhenBookmarkAlreadyExists() throws MalformedURLException {
         Bookmark bookmark = new Bookmark("https://www.orf.at");
         BookmarkTool bookmarkTool = new BookmarkTool();
